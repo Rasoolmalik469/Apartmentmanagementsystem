73f64de1d9e38d908c7e7b12b2d3283859aade90
Rasool <rasoolmalik4690@gmail.com>
2024-09-05 00:15:43 -0400

    updated

--- src/main/java/org/appartment/controller/AmenityController.java
+++ src/main/java/org/appartment/controller/AmenityController.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/** Rasool Malik Vempalli **/
+/** Rasool Malik Vempalli 1**/
 
 @Controller
 public class AmenityController {
